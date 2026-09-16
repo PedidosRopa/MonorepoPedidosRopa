eaad9093fa3d8d2788b5cedf33cfa8e771724c62
DebugggMax <max.olguin@duocuc.cl>
2026-09-16 17:06:56 -0300

    fix: agrego solo ruta de dominio

--- cloudnative/src/main/java/com/ev1/cloudnative/config/SecurityConfig.java
+++ cloudnative/src/main/java/com/ev1/cloudnative/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200", "*"));
+        configuration.setAllowedOriginPatterns(List.of("https://main.d3b5udaeum350v.amplifyapp.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
